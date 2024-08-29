098c95851f5e5b938255409bfd459a17d5cd829a
魏清柠 <13428638+wei-qingling@user.noreply.gitee.com>
2024-08-29 09:21:22 +0000

    chatbot.spa
    Signed-off-by: 魏清柠 <13428638+wei-qingling@user.noreply.gitee.com>

---
+++ 2024/NLP/chatbot/spa/classifiers.py
@@ -0,0 +1,124 @@
+﻿import os
+import pickle
+import numpy as np
+
+from spa.feature_extraction import ChiSquare
+
+# ################################################
+# 基于SVM的情感极性分析
+# ################################################
+from sklearn.svm import SVC
+
+class SVMClassifier:
+    def __init__(self, train_data=None, train_labels=None, feature_num=5000, C=50):
+        '''
+        初始化SVM分类器：
+        检查预训练模型是否存在，不存在则进行模型训练并保存；存在则直接加载模型。
+        使用定义的ChiSquare进行特征选择。
+        
+        param: train_data: 训练数据集，每条数据为一个文本样本的词语列表。
+        param: train_labels: 训练数据集对应的标签列表（0/1）。
+        param: feature_num: 选择的特征数量，默认为5000。
+        param: C: SVM的正则化参数，默认为50。
+        '''
+        # 当前执行目录
+        rootdir = os.getcwd()
+        model_path = os.path.join(rootdir, "spa/svm_model.pkl")
+        print(model_path)
+
+        if not os.path.exists(model_path):
+            # feature extraction
+            fe = ChiSquare(train_data, train_labels)
+            self.best_words = fe.best_words(feature_num)
+
+            train_data = np.array(train_data)
+            train_labels = np.array(train_labels)
+            
+            self.C=C
+            
+            self.clf = SVC(C=self.C, probability=True)
+            self.__train(train_data, train_labels)
+            self.__save_model(model_path)
+        else:
+            self.__load_model(model_path)
+            
+    def words2vector(self, all_data):
+        '''
+        将文本数据转化为向量形式。
+        
+        param: all_data: 文本数据列表，每个元素为一个文本样本的词语列表。
+        
+        return: vectors: 转换后的向量列表，每个向量对应一个样本。
+        '''
+        vectors = []
+
+        best_words_index = {}
+        for i, word in enumerate(self.best_words):
+            best_words_index[word] = i
+
+        for data in all_data:
+            vector = [0 for x in range(len(self.best_words))]
+            for word in data:
+                i = best_words_index.get(word)
+                if i is not None:
+                    vector[i] = vector[i] + 1
+            vectors.append(vector)
+
+        vectors = np.array(vectors)
+        return vectors
+
+    def __train(self, train_data, train_labels):
+        '''
+        训练SVM模型。
+        
+        param: train_data: 训练数据的向量化表示。
+        param: train_labels: 训练数据的标签列表。
+        '''
+        print("SVMClassifier is training ...... ")
+
+        train_vectors = self.words2vector(train_data)
+
+        self.clf.fit(train_vectors, np.array(train_labels))
+
+        print("SVMClassifier trains over!")
+
+    def classify(self, data):
+        '''
+        对单个样本进行分类预测。
+        
+        param: data: 单个文本样本的词语列表。
+        
+        return: proba: 预测为正类的概率。
+        return: prediction: 预测的类别标签。
+        '''
+        vector = self.words2vector([data])
+
+        prediction = self.clf.predict(vector)
+        # print(self.clf.predict_proba(vector))
+
+        return self.clf.predict_proba(vector)[0][1], prediction[0]
+
+    def __save_model(self, file_path):
+        '''
+        保存模型至文件。
+        
+        param: file_path: 模型保存的文件路径。
+        '''
+        model_data = {
+            'best_words': self.best_words,
+            'C': self.C
+        }
+        with open(file_path, 'wb') as f:
+            pickle.dump((model_data, self.clf), f)
+
+    def __load_model(self, file_path):
+        '''
+        从文件加载模型。
+        
+        param: file_path: 模型文件的路径。
+        '''
+        with open(file_path, 'rb') as f:
+            model_data, clf = pickle.load(f)
+        self.best_words = model_data['best_words']
+        self.C = model_data['C']
+        self.clf = clf
---
+++ 2024/NLP/chatbot/spa/feature_extraction.py
@@ -0,0 +1,76 @@
+﻿# 特征提取
+
+class ChiSquare:
+    def __init__(self, doc_list, doc_labels):
+        '''
+        初始化ChiSquare类：（基于卡方检验进行特征选择）
+        统计所有文档、正类文档和负类文档中各词语出现的频次。
+        计算每个词语的卡方统计量，用于后续特征选择。
+        
+        param: doc_list: 文档列表，每个文档是一个词语列表。
+        param: doc_labels: 每个文档对应的标签列表，通常1代表正类，0代表负类。
+        '''
+        self.total_data, self.total_pos_data, self.total_neg_data = {}, {}, {}
+
+        for i, doc in enumerate(doc_list):
+            if doc_labels[i] == 1:
+                for word in doc:
+                    self.total_pos_data[word] = self.total_pos_data.get(word, 0) + 1
+                    self.total_data[word] = self.total_data.get(word, 0) + 1
+            else:
+                for word in doc:
+                    self.total_neg_data[word] = self.total_neg_data.get(word, 0) + 1
+                    self.total_data[word] = self.total_data.get(word, 0) + 1
+
+        total_freq = sum(self.total_data.values())
+        total_pos_freq = sum(self.total_pos_data.values())
+        # total_neg_freq = sum(self.total_neg_data.values())
+
+        self.words = {}
+
+        for word, freq in self.total_data.items():
+            pos_score = self.__calculate(self.total_pos_data.get(word, 0), freq, total_pos_freq, total_freq)
+            # neg_score = self.__calculate(self.total_neg_data.get(word, 0), freq, total_neg_freq, total_freq)
+            self.words[word] = pos_score * 2
+
+    @staticmethod
+    def __calculate(n_ii, n_ix, n_xi, n_xx):
+        '''
+        静态方法，计算给定频次的卡方统计量。
+        
+        param: n_ii: 在正类中且属于特定词的频次。
+        param: n_ix: 在正类中但不属于特定词的频次。
+        param: n_xi: 不在正类中但属于特定词的频次。
+        param: n_xx: 不在正类中也不属于特定词的频次。
+        
+        return: chi_square: 卡方统计量。
+        '''
+        n_ii = n_ii
+        n_io = n_xi - n_ii
+        n_oi = n_ix - n_ii
+        n_oo = n_xx - n_ii - n_oi - n_io
+        return n_xx * (float((n_ii*n_oo - n_io*n_oi)**2) /
+                       ((n_ii + n_io) * (n_ii + n_oi) * (n_io + n_oo) * (n_oi + n_oo)))
+
+    def best_words(self, num, need_score=False):
+        '''
+        选取卡方统计量最高的num个词语作为特征。
+        
+        param: num: 需要选择的特征词数量。
+        param: need_score: 是否同时返回词语及其卡方得分，默认False只返回词语。
+        
+        return: selected_words: 选中的词语列表。如果need_score为True，则同时返回每个词语的卡方得分。
+        '''
+        words = sorted(self.words.items(), key=lambda word_pair: word_pair[1], reverse=True)
+
+        if need_score:
+            return [word for word in words[:num]]
+        else:
+            return [word[0] for word in words[:num]]
+
+
+
+
+
+
+
